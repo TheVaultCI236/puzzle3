bbd3cac8b73603e796384900b732e92a8653ef4a
TheVaultCI236 <50559307+TheVaultCI236@users.noreply.github.com>
2019-05-13 00:17:11 +0100

    Add files via upload

---
+++ Puzzle3.java
@@ -0,0 +1,19 @@
+
+public class Puzzle3 {
+
+	
+		public static void main(String args[]) {
+
+			boolean securityAccess = true; 
+			
+			int codeAccess = 125432;
+			
+			// Print to the system the variables and
+			// remember, this has to be in one line
+			// to ensure the security guards don't notice
+			// anything suspicious. 
+			
+	
+	}
+}
+
